fd6c211fbb7f258fc6ae8407d5ca36d2532f3a17
Moises Garcia <moygaga99@gmail.com>
2024-06-21 12:36:59 -0600

    Implementando mvc

--- src/main/java/com/example/ControladorRest.java
+++ src/main/java/com/example/ControladorRest.java
@@ -1,13 +1,27 @@
 package com.example;
 
+import com.example.domine.Individuo;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class ControladorRest {
+    @Value("${indice.hola2}")
+    private String hola2;
     @GetMapping("/")
-    public String comienzo(){
+    public String comienzo(Model model){
+        String hola = "Hola mundo con spring boot";
+        Individuo individuo = new Individuo();
+        individuo.setNombre("Moises");
+        individuo.setApellido("Garcia");
+        individuo.setEdad("24");
+        individuo.setCorreo("moy@gmail.com");
+        individuo.setTelefono("7773029751");
+        model.addAttribute("hola", hola);
+        model.addAttribute("hola2", hola2);
+        model.addAttribute("individuo", individuo);
         return "indice";
     }
 
